05f161ac8249798355a530e5e799d8a0269c3acd
Sophia Lee <sophx.0573@gmail.com>
2025-02-25 15:05:17 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+# "/" = home page; runs main at home page (in the broswer url, it's what comes after it)
+# eg. 127.0.0.1:5000/ (this is main)
+def main():
+    a = [1, 3, "h", 4] # printing out an example list
+
+    return render_template("base.html", a=a, test=True)
+    # converts jinja page to html, passing variable into jinja (jinja just contains the python code within html
+    # a=a is passing the variable into the html, while test=True is for the "Hello world !!" to show if test=True
+
+@app.route("/2")
+# this would be from 127.0.0.1:5000/2 instead of just the "/"
+def secondpage():
+    return render_template("secondpage.html")
+
+@app.route("/3")
+def thirdpage():
+    return render_template("filename.html")
+
+# just an ending thing 
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/index.js
@@ -0,0 +1,10 @@
+// defining variables
+let a = 5; 
+b = 1
+
+// def func() equivalent in JS
+function test() {
+    
+}
+
+// two types of equal; == vs === ; use the === (python "==" equivalent)
