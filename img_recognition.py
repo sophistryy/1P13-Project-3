27d0de1cb0a48467775446ab0f1bda128c283a85
Sophia Lee <sophx.0573@gmail.com>
2025-03-03 11:36:38 -0500

    changes 1

--- img_recognition.py
+++ img_recognition.py
@@ -124,11 +124,11 @@ def main():
 	for i in range(len(note_groupings)):
 		for n in range(len(note_groupings[i])):
 			note = note_groupings[i][n]
-			note_y = sorted(note_groupings[i], key=lambda n: n[1])
-			prev_note = note_y[n - 1]
+			# note_y = sorted(note_groupings[i], key=lambda n: n[1])
+			prev_note = note_groupings[i][n - 1]
 			current_staff = staff[i]
 			note_index = 0
-
+			
 			# 15-20, 2 notes | 12-14, single notes
 
 			for l in range(len(current_staff)):
@@ -153,7 +153,7 @@ def main():
 				note.append([notes[note_index]])
 					
 			elif note[2] >= 15:
-				if abs(note_y[i][0] - prev_note[0]) < 20: #double notes (15-20)
+				if abs(note[0] - prev_note[0]) < 50: #double notes (15-20)
 					note.append([notes[note_index - 1], notes[note_index + 1]])
 				else:
 					note.append([notes[note_index], notes[note_index + 1]])
@@ -169,11 +169,10 @@ def main():
 		for note in line:
 			x = note[0]
 			y = note[1]
-			inc = 20
+			inc = 15
 			h = 0
 			for i in range(len(note[-1])):
 				cv2.putText(im_with_keypoints, str(note[-1][i]), (x + inc, y + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
-				# inc += 15
 				h += 12
 
 	# plt.imshow(im_with_keypoints)
