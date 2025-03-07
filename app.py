61462857100c9991f7e82606263f7420c6662abb
Sophia Lee <sophx.0573@gmail.com>
2025-03-06 22:24:41 -0500

    Image recognition (#10)
    - added pre_processing functions
    - IR can somewhat recognize more sheets now
    - added a database and user login
    - more test images

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
+from flask_login import login_user, current_user, LoginManager, logout_user
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 from image_recognition.img_recognition import main
+from models import db, User, SavedMusic
+import json
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.secret_key = "secret"
+
+app.jinja_env.auto_reload = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+login_manager.login_view = "main_page"
+login_manager.init_app(app)
+
+db.init_app(app)
+migrate.init_app(app, db)
+bcrypt.init_app(app)
 
 @app.route("/")
 # "/" = home page; runs main at home page (in the broswer url, it's what comes after it)
@@ -30,10 +52,66 @@ def get_json():
 
     # notes = main("temp.png")
 
+    # saving data to user
+    # notes = main("image") # json data
+    # if current_user.is_authenticated:
+    #     text_data = json.dumps(notes)
+    #     music_obj = SavedMusic(user_id=current_user.id, data=text_data)
+    #     db.session.add(music_obj)
+    #     db.session.commit()
+
     return {
         "notes": main()
-        # "notes": [[["C4"],["C5", "A4","B5"],["F5"]], [["C4"],["C5", "A4","B5"],["F5"]]]
     }
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    username = request.form.get("username").lower()
+    password = request.form.get("password")
+    user = User.query.filter_by(username=username).first()
+
+    if user is not None:
+        return render_template("register.html", error="That username is already taken")
+    
+    user = User(username=username, password=bcrypt.generate_password_hash(password))
+    db.session.add(user)
+    db.session.commit()
+
+    login_user(user)
+
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+
+    return redirect("/")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    username = request.form.get("username").lower()
+    password = request.form.get("password")
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        return render_template("login.html", error="That username does not exist")
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return render_template("login.html", error="Incorrect password")
+    
+    login_user(user)
+
+    return redirect("/")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 # just an ending thing 
 if __name__ == "__main__":
---
+++ image_recognition/pre_processing.py
@@ -0,0 +1,162 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+
+img = "image_recognition\\ir_tests\\Asturias.png"
+
+def resize(image):
+	"""resizes image"""
+	base_width = 1400
+	og_img = Image.open(image).convert('RGB')
+	wpercent = (base_width / float(og_img.size[0]))
+	hsize = int((float(og_img.size[1]) * float(wpercent)))
+	resized_img = og_img.resize((base_width, hsize), Image.Resampling.LANCZOS)
+	resized_img = np.array(resized_img)[:, :, ::-1].copy()
+
+	return resized_img
+
+def blob_detector(image):
+	"""detects blobs from the image to find notes and returns a list of note coordiantes and blob size"""
+	# applying filters
+	img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# blur_img = cv2.medianBlur(img, 5)
+	# th3 = cv2.adaptiveThreshold(blur_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+	LoG = cv2.GaussianBlur(img, (13, 13), 0)
+
+	# blob detector (circles)
+	params = cv2.SimpleBlobDetector_Params()
+
+	# settings for blob detector
+	params.filterByColor = True
+	params.blobColor = 0
+ 
+	params.filterByArea = True
+	params.minArea = 130
+	params.maxArea = 600
+
+	params.filterByCircularity = True
+	params.minCircularity = 0.3
+	params.maxCircularity = 3.4028234663852886e+38
+
+	params.filterByConvexity = True
+	params.minConvexity = 0.8
+	params.maxConvexity = 3.4028234663852886e+38
+
+	params.filterByInertia = True
+	params.minInertiaRatio = 0.1
+	params.maxInertiaRatio = 0.8
+
+	params.minThreshold = 0
+	params.maxThreshold = 110
+	params.thresholdStep = 4
+	params.minDistBetweenBlobs = 0.01e-38
+	params.minRepeatability = 2
+	
+	# creating detector + detecting blobs
+	detector = cv2.SimpleBlobDetector_create(params)
+	keypoints = detector.detect(LoG)
+	
+	# creates new image with blobs circled
+	im_with_keypoints = cv2.drawKeypoints(LoG, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+	note_data = []
+	for kp in keypoints:
+		x = int(kp.pt[0])
+		y = int(kp.pt[1])
+		s = int(kp.size)
+
+		note_data.append([x,y,s])
+
+	# sorting by x-coordinate
+	note_data = sorted(note_data, key=lambda n: n[0])
+
+	# plt.imshow(im_with_keypoints)
+	# plt.show()
+
+	return note_data
+
+def line_detector(image):
+	"""detects the lines on sheet music and outputs a list of grouped line coordinates"""
+
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	kernel_size = 5
+	# blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+	th3 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+        cv2.THRESH_BINARY,11,2)
+	low_threshold = 50
+	high_threshold = 150
+	edges = cv2.Canny(th3, low_threshold, high_threshold)
+
+	rho = 1  # distance resolution in pixels of the Hough grid
+	theta = np.pi / 180  # angular resolution in radians of the Hough grid
+	threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+	min_line_length = 300  # minimum number of pixels making up a line
+	max_line_gap = 20 # maximum gap in pixels between connectable line segments
+	line_image = np.copy(image)  # creating a blank to draw lines on
+
+	# Run Hough on edge detected image
+	# Output "lines" is an array containing endpoints of detected line segments
+	lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+	
+	# sorting lines by y1 coordinate (x[0][1])
+	lines = sorted(lines, key=lambda x: x[0][1])
+
+	# getting rid of extra lines if they are too close to each other
+	unduplicated_lines = [ lines[0] ]
+	for i in range(1, len(lines)):
+		if abs(lines[i][0][1] - unduplicated_lines[-1][0][1]) > 5:
+			unduplicated_lines.append(lines[i])
+	
+	# grouping line into sections (staff or tab groups)
+	line_groupings = [ [ unduplicated_lines[0] ] ]
+	for i in range(1, len(unduplicated_lines)):
+		prev_line = line_groupings[-1][-1]
+		next_line = unduplicated_lines[i]
+
+		if abs(next_line[0][1] - next_line[0][3]) > 10:
+			continue
+		if next_line[0][1] - prev_line[0][1] < 50:
+			line_groupings[-1].append(next_line)
+		else:
+			line_groupings.append([ next_line ])
+	
+	staff = []
+	# [[x1, y1, x2, y2]]
+	for e in line_groupings:
+		if len(e) == 5:
+			staff.append([a[0] for a in e])
+
+	# determining endpoints of lines
+	line_x_left = []
+	for group in staff:
+		for line in group:
+			line_x_left.append(line[0])
+		
+	line_x_right = []
+	for group in staff:
+		for line in group:
+			line_x_right.append(line[2])
+	
+	left_x = max(set(line_x_left), key=line_x_left.count)
+	right_x = max(set(line_x_right), key=line_x_right.count)
+
+	for group in staff:
+		for line in group:
+			line[0] = left_x
+			line[2] = right_x
+			line[1] += 2
+			line[3] += 2
+
+	for group in staff:
+		for line in group:
+			cv2.line(line_image,(line[0], line[1]),(line[2], line[3]),(255,0,0),1)
+	
+	plt.imshow(line_image)
+	plt.show()
+	
+	return staff
+
+if __name__ == "__main__":
+	blob_detector(resize(img))
+	line_detector(resize(img))
